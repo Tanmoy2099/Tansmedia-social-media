496fa458d352a2cc75522225ac422544d05eb6a8
Tanmoy Nath <tanmoy.nath270@gmail.com>
2022-12-14 21:25:33 +0530

    Update baseURl again

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 // const pureBaseUrl = process.env.NODE_ENV !== "production" ? 'http://localhost:3000' : 'https://tansmedia.herokuapp.com';
 
-const Url = process.env.URL || 'https://tansmedia.onrender.com'
+const Url = process.env.URL || 'https://tansmedia-59ne.onrender.com'
 
 const pureBaseUrl = process.env.NODE_ENV !== "production" ? 'http://localhost:3000' : Url;
 
