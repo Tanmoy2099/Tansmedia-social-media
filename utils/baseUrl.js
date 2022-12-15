c239b32b70702f42008dc40e64132f3f93e49fb3
Tanmoy Nath <tanmoy.nath270@gmail.com>
2022-12-15 18:25:07 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
-const url = process.env.URL
+const url = process.env.URL || 'https://tansmedia.onrender.com'
 const pureBaseUrl = process.env.NODE_ENV !== "production" ? 'http://localhost:3000' : url;
 
 const baseUrl = `${pureBaseUrl}/api/v1`;
